de1cba1cd65de98a59227a39338ecc54ad073181
Lee H <lee@leeh.uk>
2018-01-20 21:30:41 +0000

    - add release docker image with local repo

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ docker create --name rpmtestrepo-c7 leehuk/rpmtestrepo:c7-build
 docker cp rpmtestrepo-c7:/home/rpmbuild/rpmtestrepo.tgz tmp/rpmtestrepo-c7.tgz
 docker rm -f rpmtestrepo-c7
 
-
+echo "Building leehuk/rpmtestrepo:centos7"
+docker build -t leehuk/rpmtestrepo:centos7 -f dockerfiles/Dockerfile-centos7.release .
