66cd1d1607c00cf6cbc5153177144c5129b4786c
Lee H <lee@leeh.uk>
2018-01-20 22:02:25 +0000

    - add centos6 builds

--- build.sh
+++ build.sh
@@ -16,6 +16,13 @@ docker_extract() {
     docker rm -f rpmtestrepo-build
 }
 
+echo "Building leehuk/rpmtestrepo:c6-build"
+docker build -t leehuk/rpmtestrepo:c6-build -f dockerfiles/Dockerfile-centos6.build . || bail "Docker initial build failed"
+docker_extract "leehuk/rpmtestrepo:c6-build" "/home/rpmbuild/rpmtestrepo.tgz" "tmp/rpmtestrepo-c6.tgz"
+
+echo "Building leehuk/rpmtestrepo:centos6"
+docker build -t leehuk/rpmtestrepo:centos6 -f dockerfiles/Dockerfile-centos6.release . || bail "Docker release build failed"
+
 echo "Building leehuk/rpmtestrepo:c7-build"
 docker build -t leehuk/rpmtestrepo:c7-build -f dockerfiles/Dockerfile-centos7.build . || bail "Docker initial build failed"
 docker_extract "leehuk/rpmtestrepo:c7-build" "/home/rpmbuild/rpmtestrepo.tgz" "tmp/rpmtestrepo-c7.tgz"
