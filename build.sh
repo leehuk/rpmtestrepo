cd269fe906e4fe12414ab15ab57400959ffc98f7
Lee H <lee@leeh.uk>
2018-01-20 21:59:23 +0000

    - move docker file extracting to function

--- build.sh
+++ build.sh
@@ -5,13 +5,20 @@ bail() {
     exit 1
 }
 
+docker_extract() {
+    image="$1"
+    file="$2"
+    outfile="$3"
+
+    docker rm -f rpmtestrepo-build 2>/dev/null
+    docker create --name rpmtestrepo-build $image
+    docker cp rpmtestrepo-build:"$file" "$outfile"
+    docker rm -f rpmtestrepo-build
+}
+
 echo "Building leehuk/rpmtestrepo:c7-build"
 docker build -t leehuk/rpmtestrepo:c7-build -f dockerfiles/Dockerfile-centos7.build . || bail "Docker initial build failed"
-
-docker rm -f rpmtestrepo-c7 2>/dev/null
-docker create --name rpmtestrepo-c7 leehuk/rpmtestrepo:c7-build
-docker cp rpmtestrepo-c7:/home/rpmbuild/rpmtestrepo.tgz tmp/rpmtestrepo-c7.tgz
-docker rm -f rpmtestrepo-c7
+docker_extract "leehuk/rpmtestrepo:c7-build" "/home/rpmbuild/rpmtestrepo.tgz" "tmp/rpmtestrepo-c7.tgz"
 
 echo "Building leehuk/rpmtestrepo:centos7"
 docker build -t leehuk/rpmtestrepo:centos7 -f dockerfiles/Dockerfile-centos7.release . || bail "Docker release build failed"
